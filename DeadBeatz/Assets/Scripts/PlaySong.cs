17c43b837eed084f698fdd1663d176f8fde6206e
Verdactel <36209800+Verdactel@users.noreply.github.com>
2020-02-26 13:06:54 -0700

    updated files

--- DeadBeatz/Assets/Scripts/PlaySong.cs
+++ DeadBeatz/Assets/Scripts/PlaySong.cs
@@ -16,14 +16,34 @@ public class PlaySong : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float[] spectrum = new float[256];
-        _audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
-        for (int i = 1; i < spectrum.Length - 1; i++)
+        //float[] spectrum = new float[512];
+        //AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+
+        //for (int i = 1; i < spectrum.Length - 1; i++)
+        //{
+        //    Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
+        //    Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+        //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
+        //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+
+        //}
+        if (_audioSource.time >= 128f && _audioSource.time < 129f)
         {
-            Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-            Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+            float[] curSpectrum = new float[1024];
+            _audioSource.GetSpectrumData(curSpectrum, 0, FFTWindow.BlackmanHarris);
+
+            float targetFrequency = 234f;
+            float hertzPerBin = (float)AudioSettings.outputSampleRate / 2f / 1024;
+            int targetIndex = (int)(targetFrequency / hertzPerBin);
+
+            string outString = "";
+            for (int i = targetIndex - 3; i <= targetIndex + 3; i++)
+            {
+                outString += string.Format("| Bin {0} : {1}Hz : {2} |   ", i, i * hertzPerBin, curSpectrum[i]);
+            }
+
+            Debug.Log(outString);
         }
     }
 }
