a25c3a88cd3c6c544f3ea2d3e2c4ed3ac2a4805c
Verdactel <36209800+Verdactel@users.noreply.github.com>
2020-03-07 13:14:55 -0700

    guys look i did a thing

--- DeadBeatz/Assets/Scripts/Audio/PeakAudioVisualizerBehaviour.cs
+++ DeadBeatz/Assets/Scripts/Audio/PeakAudioVisualizerBehaviour.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class PeakAudioVisualizerBehaviour : IAudioVisualizationBehaviour
 {
-
     private Animator _animator;
 
     private bool _lastWasPeak;
+
+    int count = 0;
 
     public void VisualizePoint([NotNull] SpectralFluxInfo point)
     {
@@ -22,6 +23,19 @@ public class PeakAudioVisualizerBehaviour : IAudioVisualizationBehaviour
             //target.SetActive(!target.activeSelf);
             //Spawn Object instead
             _animator.SetTrigger("Flash");
+        }
+    }
+
+    public void SpawnMonster([NotNull] SpectralFluxInfo point, ObjectPooler objectPooler, string tag)
+    {
+        if (count > 20 && point.IsPeak)
+        {
+            objectPooler.SpawnFromPool(tag, new Vector3(0, 3, 0), Quaternion.identity);
+            count = 0;
+        }
+        else
+        {
+            count++;
         }
     }
 }
