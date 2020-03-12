c0fc06c860aafc42de31325c6ac704ee6e4ae9df
Verdactel <36209800+Verdactel@users.noreply.github.com>
2020-03-12 12:57:49 -0600

    added script

--- DeadBeatz/Assets/Scripts/TouchScript.cs
+++ DeadBeatz/Assets/Scripts/TouchScript.cs
@@ -18,26 +18,14 @@ public class TouchScript : MonoBehaviour
                 RaycastHit hit;
                 if (Physics.Raycast(ray, out hit, 20, layermask) && hit.collider.gameObject == this.gameObject)
                 {
-                    if(hit.collider.tag == "CubeLeft")
-                    {
-                        GameObject go = lm.GetFirstIndex();
-                        MonsterObject mo = go.GetComponentInChildren<MonsterObject>();
-                        Debug.Log(mo.currentState);
-                        //call score on object
+                    GameObject go = lm.GetFirstIndex();
+                    MonsterObject mo = go.GetComponentInChildren<MonsterObject>();
+                    Debug.Log(mo.currentState);
+                    //call score on object
 
-                        lm.RemoveFromList(go);
-                        go.SetActive(false);
-                    }
-                    else if(hit.collider.tag == "CubeMid")
-                    {
-                        GameObject go = lm.GetFirstIndex();
-                        MonsterObject mo = go.GetComponentInChildren<MonsterObject>();
-                        Debug.Log(mo.currentState);
-                        //call score on object
+                    lm.RemoveFromList(go);
+                    go.SetActive(false);
 
-                        lm.RemoveFromList(go);
-                        go.SetActive(false);
-                    }
                 }
             }
         }
